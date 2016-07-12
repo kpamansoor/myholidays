6213eadc6d02cf97fa1e27805ed9f85b7dbfe956
kpamansoor <pingtomansoor@gmail.com>
2016-07-12 17:14:21 +0800

    client first commit

---
+++ client/holiday/www/app.js
@@ -0,0 +1,13 @@
+var App = angular.module('holiday', ['ui.router']);
+
+App.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    // For any unmatched url, send to /business
+    $urlRouterProvider.otherwise("/business")
+
+    $stateProvider
+        .state('/', { //State demonstrating Nested views
+            url: "/business",
+            templateUrl: "pages/home.html",
+            controller: "homeController"
+        })
+}]);
---
+++ client/holiday/www/js/controllers.js
@@ -0,0 +1,7 @@
+App.controller('homeController', function ($scope, $rootScope) {
+    // create a message to display in our view
+
+    $scope.list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+});
